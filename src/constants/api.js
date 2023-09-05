455341fe1f3137d22993c19f83ab9b162901b357
kolimov0825@gmail.com <kolimov0825@gmail.com>
2023-09-06 00:45:32 +0500

    API_BASE_URL changed

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
-// const API_ADDRESS1 = "16.171.65.106";
-const API_ADDRESS2 = "localhost:3000";
+const API_ADDRESS1 = "16.171.65.106";
+// const API_ADDRESS2 = "localhost:3000";
 
-export const API_BASE_URL = `http://${API_ADDRESS2}/api`;
-export const URL_IMAGE = `http://${API_ADDRESS2}`;
+export const API_BASE_URL = `http://${API_ADDRESS1}/api`;
+export const URL_IMAGE = `http://${API_ADDRESS1}`;
 
 export const STRIPE_PK =
   "pk_test_51NXavzDU6UVW8TMJ74sw5Y8EvVk6ZYKvoNIHpC0VPllpheTt0a7JOamgZc4gwnjejvfxy5tCTuziZ4j2eqTnlZVC00V4861pDX";
