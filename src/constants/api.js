9fa1cfe95893ce49d1b78176332103f40980de78
Komronbek Olimov <116647964+komronbeko@users.noreply.github.com>
2024-11-20 10:53:45 +0000

    url changed

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
-const API_ADDRESS = "sello-backend.oty2.com";
-// const API_ADDRESS = "localhost:4000";
+const API_ADDRESS = "https://sello-backend.oty2.com";
+// const API_ADDRESS = "http://localhost:4000";
 
-export const API_BASE_URL = `http://${API_ADDRESS}/api`;
-export const URL_IMAGE = `http://${API_ADDRESS}/uploads`;
+export const API_BASE_URL = `${API_ADDRESS}/api`;
+export const URL_IMAGE = `${API_ADDRESS}/uploads`;
 
 export const STRIPE_PK =
   "pk_test_51NXavzDU6UVW8TMJ74sw5Y8EvVk6ZYKvoNIHpC0VPllpheTt0a7JOamgZc4gwnjejvfxy5tCTuziZ4j2eqTnlZVC00V4861pDX";
